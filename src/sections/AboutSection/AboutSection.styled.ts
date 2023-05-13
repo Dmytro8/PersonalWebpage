5d0cb9aa87958a75ebe4e632573277f2ddb5a0e0
Dmytro Patalakha <dm.patalakha@gmail.com>
2023-05-13 20:04:02 +0200

    add smooth effect for the avatar image

--- src/sections/AboutSection/AboutSection.styled.ts
+++ src/sections/AboutSection/AboutSection.styled.ts
@@ -1,86 +1,101 @@
-import { InnerContainer } from "./../../styles/common";
-import styled from "styled-components";
-import { motion } from "framer-motion";
-export const AboutSectionContainer = styled.section`
-  display: grid;
-  justify-content: center;
-  align-items: start;
-  gap: 100px;
+import { InnerContainer } from './../../styles/common';
+import styled from 'styled-components';
+import { motion } from 'framer-motion';
 
-  @media screen and (max-width: 800px) {
-    gap: 100px;
-  }
-  button {
-    padding: 1.4rem 0;
-  }
+export const AboutSectionContainer = styled.section`
+	display: grid;
+	justify-content: center;
+	align-items: start;
+	gap: 100px;
+
+	@media screen and (max-width: 800px) {
+		gap: 100px;
+	}
+	button {
+		padding: 1.4rem 0;
+	}
 `;
 
 export const AboutSectionDescription = styled(InnerContainer)`
-  display: grid;
-  grid-template-columns: minmax(350px, 1fr) 1fr;
-  @media screen and (max-width: 1024px) {
-    grid-template-columns: 1fr;
-  }
-  @media screen and (max-width: 800px) {
-    grid-template-columns: 1fr;
-  }
+	display: grid;
+	grid-template-columns: minmax(350px, 1fr) 1fr;
+	@media screen and (max-width: 1024px) {
+		grid-template-columns: 1fr;
+	}
+	@media screen and (max-width: 800px) {
+		grid-template-columns: 1fr;
+	}
 `;
 
 export const AboutSectionMore = styled(InnerContainer)`
-  display: grid;
-  gap: 20px;
-  @media screen and (max-width: 800px) {
-    gap: 10px;
-  }
+	display: grid;
+	gap: 20px;
+	@media screen and (max-width: 800px) {
+		gap: 10px;
+	}
 `;
 
 export const AboutSectionMe = styled(motion.div)`
-  img {
-    width: 85%;
-    object-fit: contain;
-  }
-  @media screen and (max-width: 1024px) {
-    text-align: center;
-  }
-  @media screen and (max-width: 800px) {
-    display: block;
-  }
+	position: relative;
+	img {
+		width: 85%;
+		object-fit: contain;
+	}
+	@media screen and (max-width: 1024px) {
+		text-align: center;
+	}
+	@media screen and (max-width: 800px) {
+		display: block;
+	}
+	&::after {
+		content: '';
+		position: absolute;
+		width: 100%;
+		height: 200px;
+		background: ${({ theme }) => `linear-gradient(
+			0deg,
+			${theme.body} 0%,
+			transparent 100%
+		)`};
+		bottom: 0;
+		left: 0;
+	}
 `;
 
-export const AboutSectioImg = styled(motion.img)``;
+export const AboutSectionImg = styled(motion.img)``;
 
 export const AboutSectionContent = styled.div`
-  display: grid;
-  gap: 50px;
-  @media screen and (max-width: 800px) {
-    gap: 20px;
-  }
+	display: grid;
+	gap: 50px;
+	@media screen and (max-width: 800px) {
+		gap: 20px;
+	}
 `;
 
 export const AboutSectionText = styled(motion.p)`
-  text-align: justify;
-  font-size: calc(14px + 0.5vw);
-  line-height: 28px;
-  font-weight: 300;
+	text-align: justify;
+	font-size: calc(14px + 0.5vw);
+	line-height: 28px;
+	font-weight: 300;
 `;
 
 export const AboutSectionSignature = styled(motion.p)`
-  font-family: "Dancing Script", cursive !important;
-  font-size: 48px;
-  line-height: 58px;
-  margin-top: 30px;
-  @media screen and (max-width: 800px) {
-    margin-top: 10px;
-  }
+	font-family: 'Dancing Script', cursive !important;
+	font-size: 48px;
+	line-height: 58px;
+	margin-top: 30px;
+	@media screen and (max-width: 800px) {
+		margin-top: 10px;
+	}
 `;
 
 export const AboutSectionMoreText = styled.p`
-  font-size: calc(16px + 0.5vw);
-  line-height: 28px;
+	font-size: calc(16px + 0.5vw);
+	line-height: 28px;
 `;
 
 export const AboutSectionDownload = styled.a`
-  text-decoration: none;
-  color: ${({ theme }) => theme.body};
-  padding: 1.4rem 3rem;
+	text-decoration: none;
+	color: ${({ theme }) => theme.body};
+	padding: 1.4rem 3rem;
 `;
--- src/sections/AboutSection/AboutSection.tsx
+++ src/sections/AboutSection/AboutSection.tsx
@@ -1,83 +1,66 @@
-import React, { useEffect } from "react";
-import { useInView } from "react-intersection-observer";
-import { useAnimation } from "framer-motion";
-import { useTranslation } from "react-i18next";
-import { opacityVariants } from "../../styles/animation";
+import React, { useEffect } from 'react';
+import { useInView } from 'react-intersection-observer';
+import { useAnimation } from 'framer-motion';
+import { useTranslation } from 'react-i18next';
+import { opacityVariants } from '../../styles/animation';
 import {
-  AboutSectionContainer,
-  AboutSectionDescription,
-  AboutSectionMore,
-  AboutSectionMe,
-  AboutSectionContent,
-  AboutSectionText,
-  AboutSectionSignature,
-  AboutSectionMoreText,
-  AboutSectionDownload,
-  AboutSectioImg
-} from "./AboutSection.styled";
-import { Title } from "../../components/Title";
-import { Button } from "../../components/Button";
-import CV from "../../static/CV_Patalakha_Dmytro.pdf";
-import me from "../../static/me.webp";
+	AboutSectionContainer,
+	AboutSectionDescription,
+	AboutSectionMore,
+	AboutSectionMe,
+	AboutSectionContent,
+	AboutSectionText,
+	AboutSectionSignature,
+	AboutSectionMoreText,
+	AboutSectionDownload,
+	AboutSectionImg,
+} from './AboutSection.styled';
+import { Title } from '../../components/Title';
+import { Button } from '../../components/Button';
+import CV from '../../static/CV_Patalakha_Dmytro.pdf';
+import me from '../../static/me.webp';
 
 const AboutSection = () => {
-  const { t } = useTranslation();
-  const controls = useAnimation();
-  const [ref, inView] = useInView();
+	const { t } = useTranslation();
+	const controls = useAnimation();
+	const [ref, inView] = useInView();
 
-  useEffect(() => {
-    if (inView) {
-      controls.start("visible");
-    }
-  }, [controls, inView]);
-  return (
-    <AboutSectionContainer id="about">
-      <AboutSectionDescription>
-        <AboutSectionMe>
-          <AboutSectioImg
-            ref={ref}
-            animate={controls}
-            initial="hidden"
-            variants={opacityVariants}
-            src={me}
-            alt="me"
-          />
-        </AboutSectionMe>
-        <AboutSectionContent>
-          <Title title={t("about.title")} size="big" />
-          <AboutSectionText
-          // ref={ref}
-          // animate={controls}
-          // initial="hidden"
-          // variants={opacityVariants}
-          >
-            {t("about.description")}
-          </AboutSectionText>
-          <AboutSectionSignature
-          // ref={ref}
-          // animate={controls}
-          // initial="hidden"
-          // variants={opacityVariants}
-          >
-            Dmytro
-          </AboutSectionSignature>
-        </AboutSectionContent>
-      </AboutSectionDescription>
-      <AboutSectionMore
-      // ref={ref}
-      // animate={controls}
-      // initial="hidden"
-      // variants={opacityVariants}
-      >
-        <AboutSectionMoreText>{t("about.need")}</AboutSectionMoreText>
-        <Button text="Download CV" size="medium">
-          <AboutSectionDownload href={CV} download>
-            {t("about.button")}
-          </AboutSectionDownload>
-        </Button>
-      </AboutSectionMore>
-    </AboutSectionContainer>
-  );
+	useEffect(() => {
+		if (inView) {
+			controls.start('visible');
+		}
+	}, [controls, inView]);
+	return (
+		<AboutSectionContainer id='about'>
+			<AboutSectionDescription>
+				<AboutSectionMe>
+					<AboutSectionImg
+						ref={ref}
+						animate={controls}
+						initial='hidden'
+						variants={opacityVariants}
+						src={me}
+						alt='me'
+					/>
+				</AboutSectionMe>
+				<AboutSectionContent>
+					<Title title={t('about.title')} size='big' />
+					<AboutSectionText>
+						{t('about.description')}
+					</AboutSectionText>
+					<AboutSectionSignature>Dmytro</AboutSectionSignature>
+				</AboutSectionContent>
+			</AboutSectionDescription>
+			<AboutSectionMore>
+				<AboutSectionMoreText>{t('about.need')}</AboutSectionMoreText>
+				<Button text='Download CV' size='medium'>
+					<AboutSectionDownload href={CV} download>
+						{t('about.button')}
+					</AboutSectionDownload>
+				</Button>
+			</AboutSectionMore>
+		</AboutSectionContainer>
+	);
 };
 
 export default AboutSection;
